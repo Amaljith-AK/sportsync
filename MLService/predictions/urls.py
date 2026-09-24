566f29ee2545eec5ddb0872a7c6b202728b96e0d
Amaljith <amaljithak12@gmail.com>
2026-09-24 17:26:03 +0530

    Updation with new api to bypass server cold start

--- MLService/predictions/urls.py
+++ MLService/predictions/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
+from django.http import JsonResponse
 from .views import predict
 
-urlpatterns = [
-    path('predict',predict,name='predict')
-]
+def health(request):
+    return JsonResponse({'status':'ok'})
 
+urlpatterns = [
+    path('predict',predict,name='predict'),
+    path('health',health,name='health')
+]
